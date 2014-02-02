73e135b89b077384fbc6525b4e63279a8c99d8b4
Kenneth Ballenegger <kenneth@ballenegger.com>
2014-02-02 02:07:05 +0000

    separate mongo data logcation from config location

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,8 @@ start_mongo () {
     mongo_cid=$(sudo docker run \
         -d \
         `# [commented out] -p 27017:27017` \
-        -v `readlink -f ./mongo`:/data \
+        -v `readlink -f /mongo`:/mongo \
+        -v /mongo:/data \
         -name mongo \
         mongo)
     if [ $? -eq 0 ]; then
