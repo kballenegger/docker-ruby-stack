e074421ac84033b17fe0987eef982a8e7826da4a
Kenneth Ballenegger <kenneth@ballenegger.com>
2014-02-02 03:33:00 +0000

    Revert "nginx log stderr"
    This reverts commit a378df1d7af8e9047077b654fb41f5ce0ff33193.

--- nginx/launch.sh
+++ nginx/launch.sh
@@ -1,2 +1,2 @@
 echo "proxy_pass http://$APP_PORT_80_TCP_ADDR:$APP_PORT_80_TCP_PORT;" >/tmp/app_proxy_pass.nginx.conf
-nginx -g "daemon off; error_log stderr;" -c /nginx/nginx.conf
+nginx -g "daemon off;" -c /nginx/nginx.conf
