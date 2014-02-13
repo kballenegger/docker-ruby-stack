c1788f8266fe4962357f8a4f8e136080bdb68600
Kenneth Ballenegger <kenneth@ballenegger.com>
2014-02-13 08:33:33 +0000

    undo broken fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -82,7 +82,6 @@ start_deployer () {
         -v `readlink -f ./conf/deployer/app`:/app \
         -v `readlink -f /app`:/docker \
         -v `which docker`:`which docker` \
-        -v `which git`:`which git` \
         -v /var/run/docker.sock:/var/run/docker.sock \
         -expose 80 \
         -name deployer-1 \
